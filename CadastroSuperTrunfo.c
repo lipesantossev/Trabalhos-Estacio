af9f9f36ba3b806fe80bd82e47a5fb97cdf4e2ff
lipesantossev <163305863+lipesantossev@users.noreply.github.com>
2025-04-02 12:13:02 -0300

    Add files via upload
    Minha entrega do trabalho nivel novato da Faculdade Descomplica. Cadastro das cartas do jogo super trunfo

---
+++ CadastroSuperTrunfo.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    // Declaração de variáveis para a primeira carta
+    char estado1 = 'S';
+    char codigo1[4] = "SP1";
+    char nomeCidade1[50] = "São Paulo";
+    int populacao1 = 12325000;
+    float area1 = 1521.11;
+    float pib1 = 699.28;
+    int pontosTuristicos1 = 50;
+    
+    // Declaração de variáveis para a segunda carta
+    char estado2 = 'R';
+    char codigo2[4] = "RJ1";
+    char nomeCidade2[50] = "Rio de Janeiro";
+    int populacao2 = 6748000;
+    float area2 = 1200.25;
+    float pib2 = 300.50;
+    int pontosTuristicos2 = 30;
+    
+    // Exibição das informações cadastradas
+    printf("\nCarta 1:\n");
+    printf("Estado: %c\n", estado1);
+    printf("Codigo: %s\n", codigo1);
+    printf("Nome da Cidade: %s\n", nomeCidade1);
+    printf("Populacao: %d\n", populacao1);
+    printf("Area: %.2f km^2\n", area1);
+    printf("PIB: %.2f bilhoes de reais\n", pib1);
+    printf("Numero de Pontos Turisticos: %d\n", pontosTuristicos1);
+    
+    printf("\nCarta 2:\n");
+    printf("Estado: %c\n", estado2);
+    printf("Codigo: %s\n", codigo2);
+    printf("Nome da Cidade: %s\n", nomeCidade2);
+    printf("Populacao: %d\n", populacao2);
+    printf("Area: %.2f km^2\n", area2);
+    printf("PIB: %.2f bilhoes de reais\n", pib2);
+    printf("Numero de Pontos Turisticos: %d\n", pontosTuristicos2);
+    
+    return 0;
+}
